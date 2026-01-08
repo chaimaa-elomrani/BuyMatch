984a977094592feb75e2df6fe4dada6c04bb653e
Chaima elomrani <chaimaelomrani6@gmail.com>
2026-01-08 16:32:02 +0100

    added the implementation of the abstract method register

--- models/User.php
+++ models/User.php
@@ -13,5 +13,22 @@ class User extends AbstractUser{
         $this->role = $role;
     }
 
- 
+    public function register(){
+        $stmt = $this->db->prepare("SELECT id FROM users WHERE email = :email");
+        $stmt->bindParam(':email', $this->email);
+        if ($stmt->execute() && $stmt->fetch()) {
+            throw new Exception("Email already registered.");
+        }
+
+        $stmt = $this->db->prepare("INSERT INTO users (nom, prenom, email, password, role) 
+        VALUES (?, ?, ?, ?, ?)");
+
+        return $stmt->execute([
+            $this->fullname, 
+            $this->email, 
+            password_hash($this->password, PASSWORD_DEFAULT), 
+            $this->role]);
+    }
+    
+   
 }
